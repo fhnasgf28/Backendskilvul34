41d990fa43f243e1ba21cfba130f925d3252a199
1910631170019_FarhanAssegaf <82705668+fhnasgf28@users.noreply.github.com>
2022-10-18 17:06:31 +0700

    membuat navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+
+const navbar = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function() {
+    navbar.classList.toggle('slide');
+});
